a87db7d511f224b12c836fcade30ba9fc7564464
Jakob Schreiner <jakob2602@gmail.com>
2017-09-13 17:15:58 +0200

    update

--- singlecellsolver/test_adex.py
+++ singlecellsolver/test_adex.py
@@ -1,18 +1,11 @@
-"""
-This demo demonstrates how one can use the basic cell solver for the
-collection of cell models available in cbcbeat.
-
-A single cell is considered with the initial conditions given by the
-model. A stimulus of 6.25 X is applied between for t in [2, 11], and
-the evolution of the variables are computed and plotted.
-"""
-
-__author__ = "Marie E. Rognes (meg@simula.no), 2013"
-
 import math
 import matplotlib.pyplot as plt
 from xalbrain import *
 import time as systime
+import seaborn as sns
+
+sns.set()
+
 # For easier visualization of the variables
 parameters["reorder_dofs_serial"] = False
 
@@ -27,49 +20,30 @@ class Stimulus(Expression):
     def __init__(self, **kwargs):
         self.t = kwargs["t"]
     def eval(self, value, x):
-        if float(self.t) >= 0 and float(self.t) <= 1:
+        if float(self.t) >= 0 and float(self.t) <= 2:
             value[0] = -1*(-100)
         else:
             value[0] = 0.0
 
-class StimulusBR(Expression):
-    "Some self-defined stimulus."
-    def __init__(self, **kwargs):
-        self.t = kwargs["t"]
-    def eval(self, value, x):
-        if float(self.t) >= 0 and float(self.t) <= 0.7:
-            value[0] = -1*(-180)
-        else:
-            value[0] = 0.0
 
 def main():
     "Solve a single cell model on some time frame."
 
     # Initialize model and assign stimulus
+    # model = Adex()
     model = AdexManual()
-    #model = Grandi_pasqualini_bers_2010()
-    #model = FitzHughNagumoManual()
-    #model = Fitzhughnagumo()
-    #model = Tentusscher_2004_mcell()
-    #model=Fenton_karma_1998_BR_altered()
-    #model=Fenton_karma_1998_MLR1_altered()
     time = Constant(0.0)
-    # model.stimulus = StimulusBR(t=time, degree=1)
-    model.stimulus = Constant(0)
+    model.stimulus = Stimulus(t=time, degree=1)
 
-    # Initialize solver
-    """
-    params = BasicSingleCellSolver.default_parameters()
-    params["theta"] = 0.5
-    params["nonlinear_variational_solver"]["newton_solver"]["linear_solver"] = "lu"
-    solver = BasicSingleCellSolver(model, time, params)
-    """
+    # Init solver
+    adex_solver = True 
     params = SingleCellSolver.default_parameters()
-    #params["theta"] = 0.5
+    if adex_solver:
+        params = SingleCellSolver.default_parameters_adex()
+
+    # params["theta"] = 0.5     # FIXME: Why not a parameter?
     params["scheme"] = "RK4"
-    #params["nonlinear_variational_solver"]["newton_solver"]["linear_solver"] = "lu"
-    solver = SingleCellSolver(model, time, params)
-    
+    solver = SingleCellSolver(model, time, params, adex_solver)
     
     # Assign initial conditions
     (vs_, vs) = solver.solution_fields()
@@ -83,21 +57,30 @@ def main():
     times = []
     values = []
     for ((t0, t1), vs) in solutions:
-        #print "Current time: %g" % t1
         times.append(t1)
         values.append(vs.vector().array())
 
     return times, values
 
+
 def plot_results(times, values, show=True):
     "Plot the evolution of each variable versus time."
-    outfile=open('variables2.dat','w')
-
-    outfile.write(str(times))
-    outfile.write('\n')
     variables = list(zip(*values))
-    plt.figure(figsize=(20, 10))
 
+    # print(variables[1])
+    fig = plt.figure()
+    # ax = fig.add_subplot(111)
+    # ax.plot(times, variables[0])
+    print(len(times), len(variables[1]))
+    ax1 = fig.add_subplot(211)
+    print("time: ", max(times))
+    ax1.plot(times, variables[0])
+    ax2 = fig.add_subplot(212)
+    ax2.plot(times, variables[2])
+    print("time: ", max(times))
+    fig.savefig("foo.pdf")
+
+    """
     rows = int(math.ceil(math.sqrt(len(variables))))
     for (i, var) in enumerate(variables):
         plt.subplot(rows, rows, i+1)
@@ -112,11 +95,10 @@ def plot_results(times, values, show=True):
     plt.savefig("variables2.pdf")
     if show:
         plt.show()
+    """
 
 if __name__ == "__main__":
     tic= systime.time()
-    (times, values) = main()
+    times, values = main()
     print(systime.time() - tic)
     plot_results(times, values, show=False)
-    
-    
